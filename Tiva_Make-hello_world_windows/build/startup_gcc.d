18782bafbac734027c9c9333c49cd7e888bc31bb
birdgang2020 <49172048+birdgang2020@users.noreply.github.com>
2020-02-26 13:04:41 -0600

    ?

---
+++ Tiva_Make-hello_world_windows/build/startup_gcc.d
@@ -0,0 +1,11 @@
+build/startup_gcc.o: src/startup_gcc.c \
+ c:\users\jbera\embedded\gnu_tools_arm_embedded\5.4_2016q3\lib\gcc\arm-none-eabi\5.4.1\include\stdbool.h \
+ c:\users\jbera\embedded\gnu_tools_arm_embedded\5.4_2016q3\lib\gcc\arm-none-eabi\5.4.1\include\stdint.h \
+ c:\users\jbera\embedded\gnu_tools_arm_embedded\5.4_2016q3\arm-none-eabi\include\stdint.h \
+ c:\users\jbera\embedded\gnu_tools_arm_embedded\5.4_2016q3\arm-none-eabi\include\machine\_default_types.h \
+ c:\users\jbera\embedded\gnu_tools_arm_embedded\5.4_2016q3\arm-none-eabi\include\sys\features.h \
+ c:\users\jbera\embedded\gnu_tools_arm_embedded\5.4_2016q3\arm-none-eabi\include\_newlib_version.h \
+ c:\users\jbera\embedded\gnu_tools_arm_embedded\5.4_2016q3\arm-none-eabi\include\sys\_intsup.h \
+ c:\users\jbera\embedded\gnu_tools_arm_embedded\5.4_2016q3\arm-none-eabi\include\sys\_stdint.h \
+ C:\Users\jbera\Embedded\TivaWare_C_Series-2.1.4.178/inc/hw_nvic.h \
+ C:\Users\jbera\Embedded\TivaWare_C_Series-2.1.4.178/inc/hw_types.h
