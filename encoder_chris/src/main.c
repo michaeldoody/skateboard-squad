7e1db78735efa9bbfadbbb8ed107ce447f658367
Christopher Andrés Tsai <christophertsai2020@u.northwestern.edu>
2020-03-01 18:40:42 -0600

    Encoder communication with Tiva works.

---
+++ encoder_chris/src/main.c
@@ -0,0 +1,146 @@
+#include <math.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include "inc/hw_gpio.h"
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+#include "inc/hw_ints.h"
+#include "driverlib/debug.h"
+#include "driverlib/fpu.h"
+#include "driverlib/gpio.h"
+#include "driverlib/pin_map.h"
+#include "driverlib/rom.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/uart.h"
+#include "utils/uartstdio.h"
+#include "driverlib/interrupt.h"
+#include "driverlib/qei.h"
+
+#ifdef DEBUG
+void
+__error__(char *pcFilename, uint32_t ui32Line)
+{
+}
+#endif
+
+volatile int pos;
+
+void
+ConfigureUART(void)
+{
+    //
+    // Enable the GPIO Peripheral used by the UART.
+    //
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+
+    //
+    // Enable UART0
+    //
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
+
+    //
+    // Configure GPIO Pins for UART mode.
+    //
+    GPIOPinConfigure(GPIO_PA0_U0RX);
+    GPIOPinConfigure(GPIO_PA1_U0TX);
+    GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
+
+    //
+    // Use the internal 16MHz oscillator as the UART clock source.
+    //
+    UARTClockSourceSet(UART0_BASE, UART_CLOCK_PIOSC);
+
+    //
+    // Initialize the UART for console I/O.
+    //
+    UARTStdioConfig(0, 115200, 16000000);
+}
+
+int main(void)
+{
+    //
+    // Enable lazy stacking for interrupt handlers.  This allows floating-point
+    // instructions to be used within interrupt handlers, but at the expense of
+    // extra stack usage.
+    //
+    FPULazyStackingEnable();
+
+    //
+    // Set the clocking to run directly from the crystal.
+    //
+    SysCtlClockSet(SYSCTL_SYSDIV_4 | SYSCTL_USE_PLL | SYSCTL_XTAL_16MHZ |
+                       SYSCTL_OSC_MAIN);
+
+    //
+    // Enable peripherals.
+    //
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOD);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_QEI0);
+
+    //
+    // Wait for the QEI0 module to be ready.
+    //
+    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_QEI0)) {}
+
+    //
+    // Unlock GPIOD7 - Like PF0 its used for NMI - Without this step it doesn't work.
+    //
+	HWREG(GPIO_PORTD_BASE + GPIO_O_LOCK) = GPIO_LOCK_KEY; // In Tiva include this is the same as "_DD" in older versions (0x4C4F434B)
+	HWREG(GPIO_PORTD_BASE + GPIO_O_CR) |= 0x80;
+	HWREG(GPIO_PORTD_BASE + GPIO_O_LOCK) = 0;
+
+    //
+    // Configure the encoder 1 pins for use by the QEI block.
+    //
+    GPIOPinConfigure(GPIO_PD6_PHA0);
+	GPIOPinConfigure(GPIO_PD7_PHB0);
+
+    //
+    // Set GPIO pins for QEI. PhA0 -> PD6, PhB0 ->PD7. I believe this sets the pull up and makes them inputs.
+    //
+	GPIOPinTypeQEI(GPIO_PORTD_BASE, GPIO_PIN_6 |  GPIO_PIN_7);
+
+    //
+	// Disable peripheral and int before configuration.
+    //
+	QEIDisable(QEI0_BASE);
+	QEIIntDisable(QEI0_BASE, QEI_INTERROR | QEI_INTDIR | QEI_INTTIMER | QEI_INTINDEX);
+
+    //
+	// Configure quadrature encoder, use an arbitrary top limit of 1000.
+    //
+	QEIConfigure(QEI0_BASE, (QEI_CONFIG_CAPTURE_A_B  | QEI_CONFIG_NO_RESET 	| QEI_CONFIG_QUADRATURE | QEI_CONFIG_NO_SWAP), 1024);  // why max 1024?? need to configure?
+
+    //
+	// Enable the quadrature encoder.
+    //
+	QEIEnable(QEI0_BASE);
+
+    // 
+	// Set position to a middle value so we can see if things are working.
+    //
+	QEIPositionSet(QEI0_BASE, 512);
+
+    //
+    // Initialize the UART.
+    //
+    ConfigureUART();
+
+        while(1)
+        {
+
+            //
+            // Read position from encoder.
+            //
+            pos = QEIPositionGet(QEI0_BASE);
+
+            //
+            // Print position from encoder.
+            //
+            UARTprintf("pos = %d\n", pos);
+
+        }
+        
+    return 0;
+}
