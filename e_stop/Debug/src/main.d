3888fbc57f2dcbdde6e1502b29b36bed720e2dea
Christopher Andrés Tsai <christophertsai2020@u.northwestern.edu>
2020-05-07 02:26:22 -0500

    Added timer int to e-stop.

--- e_stop/Debug/src/main.d
+++ e_stop/Debug/src/main.d
@@ -12,24 +12,21 @@ src/main.obj: C:/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/mac
 src/main.obj: C:/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/machine/_stdint.h
 src/main.obj: C:/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/sys/_stdint.h
 src/main.obj: C:/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/stdbool.h
-src/main.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_types.h
-src/main.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_gpio.h
 src/main.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/pin_map.h
-src/main.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/sysctl.c
-src/main.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_ints.h
-src/main.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_nvic.h
-src/main.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_sysctl.h
-src/main.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_flash.h
-src/main.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/cpu.h
-src/main.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/debug.h
-src/main.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/interrupt.h
 src/main.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/sysctl.h
 src/main.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/gpio.c
+src/main.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_gpio.h
+src/main.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_ints.h
 src/main.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_memmap.h
+src/main.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_sysctl.h
+src/main.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_types.h
+src/main.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/debug.h
 src/main.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/gpio.h
+src/main.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/interrupt.h
+src/main.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/uart.h
+src/main.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/timer.h
 src/main.obj: C:/ti/TivaWare_C_Series-2.1.4.178/utils/uartstdio.h
 src/main.obj: C:/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/stdarg.h
-src/main.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/uart.h
 
 ../src/main.c: 
 C:/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/stdint.h: 
@@ -43,21 +40,18 @@ C:/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/machine/_types.h:
 C:/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/machine/_stdint.h: 
 C:/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/sys/_stdint.h: 
 C:/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/stdbool.h: 
-C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_types.h: 
-C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_gpio.h: 
 C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/pin_map.h: 
-C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/sysctl.c: 
-C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_ints.h: 
-C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_nvic.h: 
-C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_sysctl.h: 
-C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_flash.h: 
-C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/cpu.h: 
-C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/debug.h: 
-C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/interrupt.h: 
 C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/sysctl.h: 
 C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/gpio.c: 
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_gpio.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_ints.h: 
 C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_memmap.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_sysctl.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_types.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/debug.h: 
 C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/gpio.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/interrupt.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/uart.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/timer.h: 
 C:/ti/TivaWare_C_Series-2.1.4.178/utils/uartstdio.h: 
 C:/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/stdarg.h: 
-C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/uart.h: 
--- e_stop/src/main.c
+++ e_stop/src/main.c
@@ -1,14 +1,12 @@
 #include <stdint.h>
 #include <stdbool.h>
-#include "inc/hw_types.h"
-#include "inc/hw_gpio.h"
+
 #include "driverlib/pin_map.h"
-#include "driverlib/sysctl.c"
 #include "driverlib/sysctl.h"
 #include "driverlib/gpio.c"
-#include "driverlib/gpio.h"
-#include "utils/uartstdio.h"
 #include "driverlib/uart.h"
+#include "driverlib/timer.h"
+#include "utils/uartstdio.h"
 
 #define LED_PERIPH SYSCTL_PERIPH_GPIOF
 #define LED_BASE GPIO_PORTF_BASE
@@ -17,7 +15,14 @@
 
 #define BUTTON_BASE GPIO_PORTF_BASE
 #define BUTTON GPIO_PIN_4
-#define BUTTON_INT GPIO_INT_PIN_4
+#define BUTTON_INT INT_GPIOF
+#define BUTTON_INT_PIN GPIO_INT_PIN_4
+
+#define TIMER_PERIPH SYSCTL_PERIPH_TIMER0
+#define TIMER_BASE TIMER0_BASE
+#define TIMER TIMER_A
+#define TIMER_INT INT_TIMER0A
+#define TIMER_TIMEOUT_INT TIMER_TIMA_TIMEOUT
 
 #ifdef DEBUG
 void __error__(char *pcFilename, uint32_t ui32Line) {}
@@ -34,53 +39,61 @@ void configure_UART() {
     GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
 
     UARTClockSourceSet(UART0_BASE, UART_CLOCK_PIOSC);
-
     UARTStdioConfig(0, 115200, 16000000);
 }
 
-//void set_sensor_state() {
-//    uint8_t terminator = 0xFF;
-//
-//    while(true) {
-//        uint8_t encoderPositionLSB = UARTgetc();
-//        uint8_t encoderPositionMSB = UARTgetc();
-//        terminator = UARTgetc();
-//
-//        if(terminator == 0) {
-//            return encoderPositionLSB + (encoderPositionMSB << 8);
-//        }
-//    }
-//}
-
-void on_button_down() {
+void button_ISR() {
     uint32_t status = GPIOIntStatus(BUTTON_BASE, true);
 
-    if(status & BUTTON_INT) {
-        GPIOPinWrite(LED_BASE, BLUE_LED, BLUE_LED);
+    if(status & BUTTON_INT_PIN) {
         stop = true;
     }
 
     GPIOIntClear(BUTTON_BASE, BUTTON);
 }
 
-int main(void) {
+void timer_ISR() {
+    if (stop == true) {
+        GPIOPinWrite(LED_BASE, BLUE_LED, BLUE_LED);
+    }
+
+    TimerIntClear(TIMER_BASE, TIMER_TIMEOUT_INT);
+}
+
+int Hz_to_counts(int Hz) {
+    return (int) 120000000/Hz;
+}
+
+int main() {
     SysCtlClockSet(SYSCTL_SYSDIV_2_5|SYSCTL_USE_PLL|SYSCTL_OSC_MAIN|SYSCTL_XTAL_16MHZ);
-    SysCtlPeripheralEnable(LED_PERIPH);
-    SysCtlDelay(3);
 
     configure_UART();
 
+    SysCtlPeripheralEnable(LED_PERIPH);
+    SysCtlPeripheralEnable(TIMER_PERIPH);
+
+    SysCtlDelay(3);
+
     GPIOPinTypeGPIOInput(BUTTON_BASE, BUTTON);
+    GPIOPinTypeGPIOOutput(LED_BASE, RED_LED | BLUE_LED);
     GPIOPadConfigSet(BUTTON_BASE, BUTTON, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPU);
 
     GPIOIntDisable(BUTTON_BASE, BUTTON);
     GPIOIntClear(BUTTON_BASE, BUTTON);
     GPIOIntTypeSet(BUTTON_BASE, BUTTON, GPIO_FALLING_EDGE);
-    IntPrioritySet(INT_GPIOF, 0);
-    GPIOIntRegister(BUTTON_BASE, on_button_down);
-    GPIOIntEnable(BUTTON_BASE, BUTTON_INT);
+    GPIOIntRegister(BUTTON_BASE, button_ISR);
+    IntPrioritySet(BUTTON_INT, 0);
+    GPIOIntEnable(BUTTON_BASE, BUTTON_INT_PIN);
 
-    GPIOPinTypeGPIOOutput(LED_BASE, RED_LED | BLUE_LED);
+    TimerConfigure(TIMER_BASE, TIMER_CFG_PERIODIC);
+    TimerLoadSet(TIMER_BASE, TIMER, Hz_to_counts(100));
+    TimerEnable(TIMER_BASE, TIMER);
+
+    TimerIntDisable(TIMER_BASE, TIMER_TIMEOUT_INT);
+    TimerIntClear(TIMER_BASE, TIMER_TIMEOUT_INT);
+    TimerIntRegister(TIMER_BASE, TIMER, timer_ISR);
+    IntPrioritySet(TIMER_INT, 64);
+    TimerIntEnable(TIMER_BASE, TIMER_TIMEOUT_INT);
 
     while(1) {
         if (stop == true) {
